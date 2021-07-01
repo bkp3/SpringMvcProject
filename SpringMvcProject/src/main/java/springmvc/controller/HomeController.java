ccf673aab3d6ee39461710b7498498306325b86e
bkp3 <bholakumar.7372@gmail.com>
2021-07-01 21:23:17 +0530

    sending data from controller to view using ModelAndView

--- SpringMvcProject/src/main/java/springmvc/controller/HomeController.java
+++ SpringMvcProject/src/main/java/springmvc/controller/HomeController.java
@@ -3,6 +3,8 @@ package springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.*;
 
 @Controller
@@ -11,24 +13,38 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home(Model model) {
 		System.out.println("This is home url");
-		
+
 		model.addAttribute("name", "Rajesh Kumar");
-		model.addAttribute("price",1234);
-		
-		ArrayList<String>city=new ArrayList<String>();
+		model.addAttribute("price", 1234);
+
+		ArrayList<String> city = new ArrayList<String>();
 		city.add("Patna");
 		city.add("Kolkata");
 		city.add("Delhi");
 		city.add("Mumbai");
-		
-		model.addAttribute("c",city);
-		
+
+		model.addAttribute("c", city);
+
 		return "index";
 	}
 
 	@RequestMapping("/about")
 	public String about() {
 		return "about";
+	}
+
+	@RequestMapping("/help")
+	public ModelAndView help() {
+
+		// creating model and view object
+		ModelAndView modelAndView = new ModelAndView();
+		
+		//setting the data
+		modelAndView.addObject("name", "Rakesh Kumar");
+		//setting the view name
+		modelAndView.setViewName("help");
+		
+		return modelAndView;
 	}
 
 }
